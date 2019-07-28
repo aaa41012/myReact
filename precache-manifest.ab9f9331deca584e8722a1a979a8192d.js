cd3df052d05a5993862d91d27a6238b835b15be6
LJINX <aaa41012@gmail.com>
2019-07-28 15:09:59 +0800

    Updates

---
+++ precache-manifest.ab9f9331deca584e8722a1a979a8192d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3236c975b4ca8e9a603b046051ee593f",
+    "url": "/myReact/index.html"
+  },
+  {
+    "revision": "00e18b59d37b02d0c4cf",
+    "url": "/myReact/static/css/main.19499519.chunk.css"
+  },
+  {
+    "revision": "5b62106e870078e59172",
+    "url": "/myReact/static/js/2.d8532028.chunk.js"
+  },
+  {
+    "revision": "00e18b59d37b02d0c4cf",
+    "url": "/myReact/static/js/main.974e1e68.chunk.js"
+  },
+  {
+    "revision": "57eee2ab60fac00578ba",
+    "url": "/myReact/static/js/runtime~main.658c2d85.js"
+  },
+  {
+    "revision": "505697adc38b762e3b9e69a1b181f33f",
+    "url": "/myReact/static/media/0.505697ad.png"
+  },
+  {
+    "revision": "31630789b93d4ab0a16cba77d05e5225",
+    "url": "/myReact/static/media/1.31630789.png"
+  },
+  {
+    "revision": "50a8de24955399414875778773392904",
+    "url": "/myReact/static/media/2.50a8de24.png"
+  },
+  {
+    "revision": "2f4cb909898f3135b1182ba1bcc0fb12",
+    "url": "/myReact/static/media/3.2f4cb909.png"
+  },
+  {
+    "revision": "a1add53af40a4d720ea000c74400c6b2",
+    "url": "/myReact/static/media/4.a1add53a.jpg"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/myReact/static/media/css.b848d5d9.png"
+  },
+  {
+    "revision": "64dd08ca9b64edd224711b30b0c48088",
+    "url": "/myReact/static/media/jquery.64dd08ca.png"
+  },
+  {
+    "revision": "637ccbf2b702b94a2cf9a3ff6cdd7dc2",
+    "url": "/myReact/static/media/js.637ccbf2.png"
+  },
+  {
+    "revision": "74c362907475d0d7f0d59d0fb662135c",
+    "url": "/myReact/static/media/sass.74c36290.png"
+  },
+  {
+    "revision": "34536f6dac0413bb78a8c745da9a3461",
+    "url": "/myReact/static/media/sourcetree.34536f6d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myReact/precache-manifest.d482af6f7140ce7f05a52dcf1ab11f1f.js"
+  "/myReact/precache-manifest.ab9f9331deca584e8722a1a979a8192d.js"
 );
 
 self.addEventListener('message', (event) => {
